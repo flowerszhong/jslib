3daac20db84b78bf3d12cab47c3919c536148aec
flowerszhong <flowerszhong@gmail.com>
2017-09-11 02:00:14 +0800

    fix issue

--- parts/content-loop.php
+++ parts/content-loop.php
@@ -1,8 +1,8 @@
 <article>
     <a href="<?php echo get_permalink(); ?>" class="image"><img src="<?php echo get_post_img(); ?>" alt=""></a>
-    <h3><?php
+    <h3><a href="<?php echo get_permalink(); ?>"><?php
     the_title();
-    ?></h3>
+    ?></a></h3>
     <p><?php the_excerpt(); ?></p>
     <ul class="actions">
         <li><a href="<?php echo get_permalink(); ?>" class="button">阅读</a></li>
